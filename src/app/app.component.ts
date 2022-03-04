b25e2bea867b620ab569f17349c8415f0b6820b8
sifuna2002 <sifunadone4704@gmail.com>
2022-03-04 10:07:57 +0300

    created service users

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import {UsersService} from './users.service'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular1';
+  constructor(private user:UsersService){
+    this.user.getData().subscribe(data=>{
+      console.warn(data);
+    })
+  }
 }
